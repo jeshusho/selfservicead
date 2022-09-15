690d29e71a445d2fe3cfcba9c0c87f15bded01f2
Jesus Angeles <jesus.angeles@gmail.com>
2022-09-15 10:39:30 -0300

    queue error

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        //$schedule->call('App\Http\Controllers\NotificationController@send')->everyMinute();
+        $schedule->call('App\Http\Controllers\NotificationController@send')->everyMinute();
     }
 
     /**
