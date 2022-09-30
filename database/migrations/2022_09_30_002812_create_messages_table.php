b031f0fb2f8e5e5417771c9d128373c6e0e2a2fb
Jesus Angeles <jesus.angeles@gmail.com>
2022-09-30 00:42:44 -0300

    agrega log mensajes

---
+++ database/migrations/2022_09_30_002812_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->biginteger('aduser_id')->unsigned()->nullable();
+            $table->foreign('aduser_id')->references('id')->on('adusers');
+            $table->dateTime('sendig_time');
+            $table->tinyInteger('days');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
